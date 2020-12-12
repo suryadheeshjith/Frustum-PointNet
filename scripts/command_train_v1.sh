ed5f1b304e0fd93ec8e87a4be49f2c7c01c16c57
suryadheeshjith <Surya.dheeshjith@gmail.com>
2020-12-13 00:39:17 +0530

    51 epochs

--- scripts/command_train_v1.sh
+++ scripts/command_train_v1.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-python train/train.py --gpu 0 --log_dir train/log_v3 --num_point 1024 --max_epoch 50 --batch_size 32 --decay_step 800000 --decay_rate 0.5 --loss quantile
+python train/train.py --gpu 0 --log_dir train/log_v3 --num_point 1024 --max_epoch 51 --batch_size 32 --decay_step 800000 --decay_rate 0.5 --loss quantile
